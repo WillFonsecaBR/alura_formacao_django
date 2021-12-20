8a4c7d487611de181227d60a93dd923d1fbc6033
Willian Alves Fonseca <willian.alves2109@gmail.com>
2021-12-19 22:55:00 -0300

    Atualizando repositorio

--- curso-django-01-introducao/receitas/views.py
+++ curso-django-01-introducao/receitas/views.py
@@ -1,7 +1,19 @@
 from django.shortcuts import render
 
 def index(request):
-    return render(request,'index.html')
+    receitas = {
+        1: 'Bolo de laranja',
+        2: 'Bolo de cenoura',
+        3: 'Guizado de frango', 
+        4: 'Browne de chocolate'
+    }
+    
+    dados = { 
+        'nome_das_receitas' : receitas
+    }
+    return render(request,'index.html',dados)
 
 def receita(request):
     return render(request,'receita.html')
+
+
